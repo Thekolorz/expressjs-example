a19c1a52577c2d8dade6649a7a7d050c176c7946
Eni Sinanaj <enisinanaj@outlook.com>
2021-01-04 02:21:29 +0100

    Step 4. Advanced paths and path parameters

--- Books.js
+++ Books.js
@@ -1,7 +1,13 @@
 const Books = [
     {
+        id: 1,
         author: "Yuval Noah Harari",
         title: "Sapiens: A brief History of Himankind."
+    },
+    {
+        id: 2,
+        author: "Eni",
+        title: "The book of a thousand mistakes."
     }
 ];
 
--- index.js
+++ index.js
@@ -6,8 +6,13 @@ const port = 3000;
 
 
 // GET
-app.get("/", (req, res) => {
+app.get("/books", (req, res) => {
     res.json(Books);
-})
+});
+
+app.get("/books/:id", (req, res) => {
+    let book = Books.find(book => book.id == req.params.id);
+    res.json(book);
+});
 
 app.listen( port, () => console.log("HTTP server listening on port " + port))
