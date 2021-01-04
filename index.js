7567affc98091aaa6f46b511d99b8a7a41580913
Eni Sinanaj <enisinanaj@outlook.com>
2021-01-04 02:10:58 +0100

    Step 2. Simple GET request handler with Express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello world!")
+const Express = require('express');
+
+const app = Express();
+const port = 3000;
+
+
+// GET
+app.get("/", (req, res) => {
+    res.send("Hello world!")
+})
+
+app.listen( port, () => console.log("HTTP server listening on port " + port))
