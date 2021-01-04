5e44bcc9b68075a26fa23844d19e2ad2777a1430
Eni Sinanaj <enisinanaj@outlook.com>
2021-01-04 02:30:04 +0100

    Step 5. POST request handler

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const Books = require('./Books')
 const app = Express();
 const port = 3000;
 
+app.use(Express.json());
+app.use(Express.urlencoded({extended: true}));
 
 // GET
 app.get("/books", (req, res) => {
@@ -14,5 +16,10 @@ app.get("/books/:id", (req, res) => {
     let book = Books.find(book => book.id == req.params.id);
     res.json(book);
 });
+
+// POST
+app.post("/testpost", (req, res) => {
+    console.log(req.body);
+})
 
 app.listen( port, () => console.log("HTTP server listening on port " + port))
